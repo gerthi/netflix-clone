f83b237e5bde7fb7834b8de8a2aeecaea9224b7e
Thibaud Gerin <thibaud.gerin@gmail.com>
2020-10-04 18:18:53 +0200

    header on browse page & dropdown menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,23 +1,71 @@
-import React from "react";
+import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Background, Container, Logo, ButtonLink } from './styles/header';
+import {
+  Background,
+  Container,
+  Logo,
+  ButtonLink,
+  Feature,
+  Text,
+  FeatureCallOut,
+  Link,
+  Group,
+  Profile,
+  Dropdown,
+  Picture,
+} from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
 }
 
-Header.Frame = function HeaderFrame({children, ...restProps}) {
+Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
-}
+};
 
-Header.ButtonLink = function HeaderButtonLink({children, ...restProps}) {
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
   return <ButtonLink {...restProps}>{children}</ButtonLink>;
-}
+};
 
-Header.Logo = function HeaderLogo({to, ...restProps}) {
+Header.Logo = function HeaderLogo({ to, ...restProps }) {
   return (
     <ReactRouterLink to={to}>
       <Logo {...restProps} />
     </ReactRouterLink>
-  )
-}
+  );
+};
+
+Header.Text = function HeadeText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.TextLink = function HeadeTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Header.Picture = function HeadePicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`images/users/${src}.png`} />;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
